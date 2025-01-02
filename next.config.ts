697e427dc0a003dc16a7b4441a899a0140010c46
HuyNQ <quanghuynt93@gmail.com>
2025-01-02 22:38:27 +0700

    Dựng lại các component và phân chia bố cục

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    // additionalData: `$var: red;`,
+    implementation: 'sass-embedded',
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,122 +1,120 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
+    <div className="ladi-container">
 
-      <div className="ladi-container">
-        <div id="section1">
-          <div className="section1-element-ladi">
-            <img
-              src="https://scontent.fhan14-4.fna.fbcdn.net/v/t39.30808-6/454896367_1180315873242956_4160721346252859083_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=wsKg6jDtuI4Q7kNvgGCAdxU&_nc_oc=Adg0hvPXaDMC-OkRu3Q_uTZY7QnKSNYv2ShpcQ5OBwekrw0lVvCc86AG8Ux-JU4LRFcb6PlyxGakhWVQsrUfXP8m&_nc_zt=23&_nc_ht=scontent.fhan14-4.fna&_nc_gid=A-aYEdsGAAWwNRTdHs8xHMB&oh=00_AYA-F3Q1ea-U_7KxZPlpD8XGUxYNM-29UWBt0ujzkOegyg&oe=677BF07C"
-              className="img-title"
-            ></img>
-            <div className="ladi-name">THẮNG MỜ CI</div>
-            <div className="ladi-tittle">XE ĐI CHUNG GIÁ RẺ </div>
-            <div className="ladi-tittle-name">HẢI PHÒNG - HÀ NỘI - HẠ LONG</div>
+      <div id="section1">
+        <div className="section1-element-ladi">
+          <img
+            src="https://scontent.fhan14-4.fna.fbcdn.net/v/t39.30808-6/454896367_1180315873242956_4160721346252859083_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=wsKg6jDtuI4Q7kNvgGCAdxU&_nc_oc=Adg0hvPXaDMC-OkRu3Q_uTZY7QnKSNYv2ShpcQ5OBwekrw0lVvCc86AG8Ux-JU4LRFcb6PlyxGakhWVQsrUfXP8m&_nc_zt=23&_nc_ht=scontent.fhan14-4.fna&_nc_gid=A-aYEdsGAAWwNRTdHs8xHMB&oh=00_AYA-F3Q1ea-U_7KxZPlpD8XGUxYNM-29UWBt0ujzkOegyg&oe=677BF07C"
+            className="img-title"
+          ></img>
+          <div className="ladi-name">THẮNG MỜ CI</div>
+          <div className="ladi-tittle">XE ĐI CHUNG GIÁ RẺ </div>
+          <div className="ladi-tittle-name">HẢI PHÒNG - HÀ NỘI - HẠ LONG</div>
+        </div>
+        <a href="tel:0769.863.555" className="ladi-image-tell">
+          <img src="https://w.ladicdn.com/s400x400/64b98a209a84c3001265d9e4/goi-ngay-20240528151454-nofqi.png"></img>
+        </a>
+
+        <div className="ladi-image"></div>
+        <a href="tel:0769.863.555" id="hotline3" className="ladi-element">
+          <h3 className="ladi-hotline-name">
+            <span>Tổng đài đặt xe:</span>
+          </h3>
+
+          <h3 className="ladi-hotline">
+            <span>0769.863.555</span>
+          </h3>
+        </a>
+      </div>
+      <div id="section2">
+        <a href="tel:0769.863.555" className="ladi-element">
+          <div className="ladi-image-background">
+            <img src="https://scontent.fhan14-3.fna.fbcdn.net/v/t39.30808-6/452201506_1168450841096126_1639755511412684892_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=vrgJhEdXY3AQ7kNvgFvmxj4&_nc_oc=Adgg1YOFd7Y8u0JTRp1Iu3I8vcmSzCXTSQyERdZ_vUOwvHIcKbPeILPT-She6CP-vaF9Q4szBs1e1DPtSs6sX0F2&_nc_zt=23&_nc_ht=scontent.fhan14-3.fna&_nc_gid=Aw0zLJRks1eGJzbvQPsvXVm&oh=00_AYBKwyJE81yeWJYzFogvZndY8oo2j7R5DQ2GYYbUcmSdog&oe=677C1F69"></img>
           </div>
-          <a href="tel:0769.863.555" className="ladi-image-tell">
-            <img src="https://w.ladicdn.com/s400x400/64b98a209a84c3001265d9e4/goi-ngay-20240528151454-nofqi.png"></img>
-          </a>
-
-          <div className="ladi-image"></div>
-          <a href="tel:0769.863.555" id="hotline3" className="ladi-element">
-            <h3 className="ladi-hotline-name">
-              <span>Tổng đài đặt xe:</span>
-            </h3>
-
-            <h3 className="ladi-hotline">
-              <span>0769.863.555</span>
-            </h3>
-          </a>
-        </div>
-        <div id="section2">
-          <a href="tel:0769.863.555" className="ladi-element">
-            <div className="ladi-image-background">
-              <img src="https://scontent.fhan14-3.fna.fbcdn.net/v/t39.30808-6/452201506_1168450841096126_1639755511412684892_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=vrgJhEdXY3AQ7kNvgFvmxj4&_nc_oc=Adgg1YOFd7Y8u0JTRp1Iu3I8vcmSzCXTSQyERdZ_vUOwvHIcKbPeILPT-She6CP-vaF9Q4szBs1e1DPtSs6sX0F2&_nc_zt=23&_nc_ht=scontent.fhan14-3.fna&_nc_gid=Aw0zLJRks1eGJzbvQPsvXVm&oh=00_AYBKwyJE81yeWJYzFogvZndY8oo2j7R5DQ2GYYbUcmSdog&oe=677C1F69"></img>
+        </a>
+      </div>
+      <div id="section3">
+        <div className="ladi-section3-container">
+          <a id="hotline4" className="ladi-element1">
+            <h3 className="ladi-hotline"> </h3>
+            Chuyên xe tiện chuyến, xe ghép
+            <br></br>
+            <div className="ladi-shape">
+              {/* <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none" viewBox="0 0 1792 1896.08" class="" fill="rgba(0, 74, 173, 1)"><use link:href="#shape_bfJKrmuWVv"></use></svg>   */}
             </div>
+            Hải Phòng - Hà Nội - Quảng Ninh
           </a>
-        </div>
-        <div id="section3">
-          <div className="ladi-section3-container">
-            <a id="hotline4" className="ladi-element1">
-              <h3 className="ladi-hotline"> </h3>
-              Chuyên xe tiện chuyến, xe ghép
-              <br></br>
-              <div className="ladi-shape">
-                {/* <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none" viewBox="0 0 1792 1896.08" class="" fill="rgba(0, 74, 173, 1)"><use link:href="#shape_bfJKrmuWVv"></use></svg>   */}
-              </div>
-              Hải Phòng - Hà Nội - Quảng Ninh
-            </a>
-            {/* <a id="hotline4" className="ladi-element2">
+          {/* <a id="hotline4" className="ladi-element2">
               <h4>Xe ghép nhanh </h4>
               <br></br>
               <h5>Hải Phòng - Hà Nội</h5>
               <h4>và ngược lại</h4>
             </a> */}
-            <a id="hotline4" className="ladi-element2">
-              <i className="fa-thin fa-hand-point-right"></i>
-              <h4> Bao xe Hà Nội - Hạ long: 1100k- 1500k</h4>
-              <h4> Bao xe Hải Phòng - Hạ long: 400k- 600k</h4>
-              <h4> Bao xe Hải Phòng - Hà Nội: 900k- 1200k</h4>
-              <h4>
-                {" "}
-                Ghép Hải Phòng - Hà Nội: Từ 400k/ghế
-                <br></br>
-                Ghép Hải Phòng Hạ long: Từ 250k/ghế
-              </h4>
-              <h5>
-                {" "}
-                Gửi hàng hỏa tốc Hải Phòng - Hà Nội - Hạ Long: chỉ từ 150k
-              </h5>
-              <h6> GIÁ TRÊN ĐÃ BAO GỒM VÉ CẦU ĐƯỜNG , SÂN BAY NẾU CÓ</h6>
-            </a>
-          </div>
-        </div>
-        <div className="section4">
-          <a href="tel:0769.863.555">
-            <h3>NHANH - MỚI - AN TOÀN - TIẾT KIỆM</h3>
-            <h4>Gọi ngay HOTLINE: 0769.863.555</h4>
-            <h4>Hoặc kết nối ZALO 24/24</h4>
-          </a>
-        </div>
-        <div className="section5">
-          <h5>
-            {" "}
-            Xe tiện chuyến hiện nay đang là sự lựa chọn hàng đầucủa mọi nhà về
-            sự tiện lợi cũng như giá thành hợp lí.
-          </h5>
-          <h5>
-            Đến với XE TIỆN CHUYẾN THẮNG MỜ CI, bạn sẽ có được những trải nghiệm
-            tuyệt vời nhất với:
-          </h5>
-          <ul>
-            <li>Dịch vụ "đón tận nơi- trả tận nhà".</li>
-            <li>Xe mới - đẹp- sạch sẽ.</li>
-            <li>Lái xe điềm đạm, nhiệt tình, chu đáo.</li>
-            <li>Giá cả hợp lí, rõ ràng.</li>
-          </ul>
-          <h5>
-            {" "}
-            XE TIỆN CHUYẾN THẮNG MỜ CI tự hào mang đến cho quý khách dịch vụ xe
-            tiện chuyến tốt nhất.
-          </h5>
-          <h5>
-            Tự hào trong kinh nghiệm nhiều năm trong lĩnh vực xe ghép, xe tiện
-            chuyến giá rẻ. Cam kết mang đến sự hài lòng cho khác hàng
-          </h5>
-        </div>
-        {/* <div className="section6">
-          <div className="ladi-review-title">PHẢN HỒI CỦA KHÁCH HÀNG</div>
-        </div> */}
-        <div className="section7">
-          <div className="ladi-footer-title">THẮNG MỜ CI XE CHUNG GIÁ RẺ </div>
-          <div className="ladi-address">địa chỉ: 72 Lạch Tray</div>
-
-          <a href="tel:0769.863.555">
-            <div className="ladi-phone">Số điện thoại: 0769.863.555</div>
+          <a id="hotline4" className="ladi-element2">
+            <i className="fa-thin fa-hand-point-right"></i>
+            <h4> Bao xe Hà Nội - Hạ long: 1100k- 1500k</h4>
+            <h4> Bao xe Hải Phòng - Hạ long: 400k- 600k</h4>
+            <h4> Bao xe Hải Phòng - Hà Nội: 900k- 1200k</h4>
+            <h4>
+              {" "}
+              Ghép Hải Phòng - Hà Nội: Từ 400k/ghế
+              <br></br>
+              Ghép Hải Phòng Hạ long: Từ 250k/ghế
+            </h4>
+            <h5>
+              {" "}
+              Gửi hàng hỏa tốc Hải Phòng - Hà Nội - Hạ Long: chỉ từ 150k
+            </h5>
+            <h6> GIÁ TRÊN ĐÃ BAO GỒM VÉ CẦU ĐƯỜNG , SÂN BAY NẾU CÓ</h6>
           </a>
         </div>
       </div>
+      <div className="section4">
+        <a href="tel:0769.863.555">
+          <h3>NHANH - MỚI - AN TOÀN - TIẾT KIỆM</h3>
+          <h4>Gọi ngay HOTLINE: 0769.863.555</h4>
+          <h4>Hoặc kết nối ZALO 24/24</h4>
+        </a>
+      </div>
+      <div className="section5">
+        <h5>
+          {" "}
+          Xe tiện chuyến hiện nay đang là sự lựa chọn hàng đầucủa mọi nhà về
+          sự tiện lợi cũng như giá thành hợp lí.
+        </h5>
+        <h5>
+          Đến với XE TIỆN CHUYẾN THẮNG MỜ CI, bạn sẽ có được những trải nghiệm
+          tuyệt vời nhất với:
+        </h5>
+        <ul>
+          <li>Dịch vụ &quot;đón tận nơi- trả tận nhà&quot;.</li>
+          <li>Xe mới - đẹp- sạch sẽ.</li>
+          <li>Lái xe điềm đạm, nhiệt tình, chu đáo.</li>
+          <li>Giá cả hợp lí, rõ ràng.</li>
+        </ul>
+        <h5>
+          {" "}
+          XE TIỆN CHUYẾN THẮNG MỜ CI tự hào mang đến cho quý khách dịch vụ xe
+          tiện chuyến tốt nhất.
+        </h5>
+        <h5>
+          Tự hào trong kinh nghiệm nhiều năm trong lĩnh vực xe ghép, xe tiện
+          chuyến giá rẻ. Cam kết mang đến sự hài lòng cho khác hàng
+        </h5>
+      </div>
+      {/* <div className="section6">
+          <div className="ladi-review-title">PHẢN HỒI CỦA KHÁCH HÀNG</div>
+        </div> */}
+      <div className="section7">
+        <div className="ladi-footer-title">THẮNG MỜ CI XE CHUNG GIÁ RẺ </div>
+        <div className="ladi-address">địa chỉ: 72 Lạch Tray</div>
+
+        <a href="tel:0769.863.555">
+          <div className="ladi-phone">Số điện thoại: 0769.863.555</div>
+        </a>
+      </div>
+    </div>
 
   );
 }
