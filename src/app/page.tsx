bb88032b3728208bf763cb7198b66304e2de1dc3
HuyNQ <quanghuynt93@gmail.com>
2025-01-02 20:05:32 +0700

    Move to new Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,121 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+      <div className="ladi-container">
+        <div id="section1">
+          <div className="section1-element-ladi">
+            <img
+              src="https://scontent.fhan14-4.fna.fbcdn.net/v/t39.30808-6/454896367_1180315873242956_4160721346252859083_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=wsKg6jDtuI4Q7kNvgGCAdxU&_nc_oc=Adg0hvPXaDMC-OkRu3Q_uTZY7QnKSNYv2ShpcQ5OBwekrw0lVvCc86AG8Ux-JU4LRFcb6PlyxGakhWVQsrUfXP8m&_nc_zt=23&_nc_ht=scontent.fhan14-4.fna&_nc_gid=A-aYEdsGAAWwNRTdHs8xHMB&oh=00_AYA-F3Q1ea-U_7KxZPlpD8XGUxYNM-29UWBt0ujzkOegyg&oe=677BF07C"
+              className="img-title"
+            ></img>
+            <div className="ladi-name">THẮNG MỜ CI</div>
+            <div className="ladi-tittle">XE ĐI CHUNG GIÁ RẺ </div>
+            <div className="ladi-tittle-name">HẢI PHÒNG - HÀ NỘI - HẠ LONG</div>
+          </div>
+          <a href="tel:0769.863.555" className="ladi-image-tell">
+            <img src="https://w.ladicdn.com/s400x400/64b98a209a84c3001265d9e4/goi-ngay-20240528151454-nofqi.png"></img>
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+
+          <div className="ladi-image"></div>
+          <a href="tel:0769.863.555" id="hotline3" className="ladi-element">
+            <h3 className="ladi-hotline-name">
+              <span>Tổng đài đặt xe:</span>
+            </h3>
+
+            <h3 className="ladi-hotline">
+              <span>0769.863.555</span>
+            </h3>
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div id="section2">
+          <a href="tel:0769.863.555" className="ladi-element">
+            <div className="ladi-image-background">
+              <img src="https://scontent.fhan14-3.fna.fbcdn.net/v/t39.30808-6/452201506_1168450841096126_1639755511412684892_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=vrgJhEdXY3AQ7kNvgFvmxj4&_nc_oc=Adgg1YOFd7Y8u0JTRp1Iu3I8vcmSzCXTSQyERdZ_vUOwvHIcKbPeILPT-She6CP-vaF9Q4szBs1e1DPtSs6sX0F2&_nc_zt=23&_nc_ht=scontent.fhan14-3.fna&_nc_gid=Aw0zLJRks1eGJzbvQPsvXVm&oh=00_AYBKwyJE81yeWJYzFogvZndY8oo2j7R5DQ2GYYbUcmSdog&oe=677C1F69"></img>
+            </div>
+          </a>
+        </div>
+        <div id="section3">
+          <div className="ladi-section3-container">
+            <a id="hotline4" className="ladi-element1">
+              <h3 className="ladi-hotline"> </h3>
+              Chuyên xe tiện chuyến, xe ghép
+              <br></br>
+              <div className="ladi-shape">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none" viewBox="0 0 1792 1896.08" class="" fill="rgba(0, 74, 173, 1)"><use link:href="#shape_bfJKrmuWVv"></use></svg>   */}
+              </div>
+              Hải Phòng - Hà Nội - Quảng Ninh
+            </a>
+            {/* <a id="hotline4" className="ladi-element2">
+              <h4>Xe ghép nhanh </h4>
+              <br></br>
+              <h5>Hải Phòng - Hà Nội</h5>
+              <h4>và ngược lại</h4>
+            </a> */}
+            <a id="hotline4" className="ladi-element2">
+              <i className="fa-thin fa-hand-point-right"></i>
+              <h4> Bao xe Hà Nội - Hạ long: 1100k- 1500k</h4>
+              <h4> Bao xe Hải Phòng - Hạ long: 400k- 600k</h4>
+              <h4> Bao xe Hải Phòng - Hà Nội: 900k- 1200k</h4>
+              <h4>
+                {" "}
+                Ghép Hải Phòng - Hà Nội: Từ 400k/ghế
+                <br></br>
+                Ghép Hải Phòng Hạ long: Từ 250k/ghế
+              </h4>
+              <h5>
+                {" "}
+                Gửi hàng hỏa tốc Hải Phòng - Hà Nội - Hạ Long: chỉ từ 150k
+              </h5>
+              <h6> GIÁ TRÊN ĐÃ BAO GỒM VÉ CẦU ĐƯỜNG , SÂN BAY NẾU CÓ</h6>
+            </a>
+          </div>
+        </div>
+        <div className="section4">
+          <a href="tel:0769.863.555">
+            <h3>NHANH - MỚI - AN TOÀN - TIẾT KIỆM</h3>
+            <h4>Gọi ngay HOTLINE: 0769.863.555</h4>
+            <h4>Hoặc kết nối ZALO 24/24</h4>
+          </a>
+        </div>
+        <div className="section5">
+          <h5>
+            {" "}
+            Xe tiện chuyến hiện nay đang là sự lựa chọn hàng đầucủa mọi nhà về
+            sự tiện lợi cũng như giá thành hợp lí.
+          </h5>
+          <h5>
+            Đến với XE TIỆN CHUYẾN THẮNG MỜ CI, bạn sẽ có được những trải nghiệm
+            tuyệt vời nhất với:
+          </h5>
+          <ul>
+            <li>Dịch vụ "đón tận nơi- trả tận nhà".</li>
+            <li>Xe mới - đẹp- sạch sẽ.</li>
+            <li>Lái xe điềm đạm, nhiệt tình, chu đáo.</li>
+            <li>Giá cả hợp lí, rõ ràng.</li>
+          </ul>
+          <h5>
+            {" "}
+            XE TIỆN CHUYẾN THẮNG MỜ CI tự hào mang đến cho quý khách dịch vụ xe
+            tiện chuyến tốt nhất.
+          </h5>
+          <h5>
+            Tự hào trong kinh nghiệm nhiều năm trong lĩnh vực xe ghép, xe tiện
+            chuyến giá rẻ. Cam kết mang đến sự hài lòng cho khác hàng
+          </h5>
+        </div>
+        {/* <div className="section6">
+          <div className="ladi-review-title">PHẢN HỒI CỦA KHÁCH HÀNG</div>
+        </div> */}
+        <div className="section7">
+          <div className="ladi-footer-title">THẮNG MỜ CI XE CHUNG GIÁ RẺ </div>
+          <div className="ladi-address">địa chỉ: 72 Lạch Tray</div>
+
+          <a href="tel:0769.863.555">
+            <div className="ladi-phone">Số điện thoại: 0769.863.555</div>
+          </a>
+        </div>
+      </div>
+
   );
 }
